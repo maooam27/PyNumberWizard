4ad907000900d4ae8b5651666d62c4dc66b91201
maooam27 <52970070+maooam27@users.noreply.github.com>
2025-06-16 20:52:43 +0200

    structure adjustments

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ fgc = "#bfbfbf"
 
 # Logic for the wizard
 def startWizard():
-    pass  # Placeholder for the wizard logic
+    currentNumber.grid(row=0, column=0)
 
 
 
@@ -35,22 +35,26 @@ currentNumber.grid(pady=10, padx=20, row=0, column=0)
 currentNumber.grid_forget()  # To make it hidden at the start of the game
 
 # Higher lower buttons
-higherButton = Button(instructionsFrame, text="Higher", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=lambda: print("Higher selected"))
+higherButton = Button(instructionsFrame, text="Higher", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=None)
 higherButton.grid(pady=10, row=1, column=0, padx=10)
 
-lowerButton = Button(instructionsFrame, text="Lower", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=lambda: print("Lower selected"))
+lowerButton = Button(instructionsFrame, text="Lower", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=None)
 lowerButton.grid(pady=10, row=1, column=1, padx=10)
 
 # Revealing the correct number
-correctBtn = Button(root, text="Correct!", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=startWizard)
+correctBtn = Button(root, text="Correct!", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=None)
 correctBtn.pack(pady=10)
 
 # Reset button to start over
-resetBtn = Button(root, text="Reset", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=lambda: print("Resetting..."))
+resetBtn = Button(root, text="Reset", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=None, width=6)
 resetBtn.place(x=700, y=20)
 
+# Start button
+startBtn = Button(root, text="Start", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=startWizard, width=6)
+startBtn.place(x=700, y=60)
+
 # Label explaining the game
-instructions =  "Try to think of a whole number between 1 and 100\n" \
+instructions =  "Try to think of a whole number between 1 and 100 then press start\n" \
                 "and I will try to guess it through a binary search!\n" \
                 "You can tell me if my guess is higher, lower, or correct.\n"
 instructionsLabel = Label(root, text=instructions, font=("Ubuntu", 16), fg=fgc, bg=bgc)
