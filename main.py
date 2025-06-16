ffcd257740a3339126ee8695b9f0a1bb7f93e378
maooam27 <52970070+maooam27@users.noreply.github.com>
2025-06-16 20:42:50 +0200

    actually made the right structure

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ fgc = "#bfbfbf"
 
 # Logic for the wizard
 def startWizard():
-    print(numberEntry.get())
+    pass  # Placeholder for the wizard logic
 
 
 
@@ -24,24 +24,39 @@ def startWizard():
 title = Label(root, text="PyNumberWizard", font=("Ubuntu", 24), fg=fgc, bg=bgc)
 title.pack(pady=20, anchor="nw", padx=20)
 
+
 # Frame for instructions
 instructionsFrame = Frame(root, bg=bgc)
 instructionsFrame.pack()
 
-# Label for number selection
-numberChoosingLabel = Label(instructionsFrame, text="Choose your number!", font=("Ubuntu", 16), fg=fgc, bg=bgc)
-numberChoosingLabel.grid(pady=10, row=0, column=0, padx=10)
+# Display current number
+currentNumber = Label(instructionsFrame, text="Thinking about number: ", font=("Ubuntu", 16), fg=fgc, bg=bgc)
+currentNumber.grid(pady=10, padx=20, row=0, column=0)
+currentNumber.grid_forget()  # To make it hidden at the start of the game
 
-# Entry for number selection
-numberEntry = Entry(instructionsFrame, font=("Ubuntu", 16), fg=fgc, bg="#2C3333", width=10)
-numberEntry.grid(pady=10, row=0, column=1, padx=10)
+# Higher lower buttons
+higherButton = Button(instructionsFrame, text="Higher", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=lambda: print("Higher selected"))
+higherButton.grid(pady=10, row=1, column=0, padx=10)
 
-# Button to submit the number
-submitBtn = Button(root, text="Submit", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=startWizard)
-submitBtn.pack(pady=10)
+lowerButton = Button(instructionsFrame, text="Lower", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=lambda: print("Lower selected"))
+lowerButton.grid(pady=10, row=1, column=1, padx=10)
 
+# Revealing the correct number
+correctBtn = Button(root, text="Correct!", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=startWizard)
+correctBtn.pack(pady=10)
 
-# TODO: validate the input
+# Reset button to start over
+resetBtn = Button(root, text="Reset", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=lambda: print("Resetting..."))
+resetBtn.place(x=700, y=20)
+
+# Label explaining the game
+instructions =  "Try to think of a whole number between 1 and 100\n" \
+                "and I will try to guess it through a binary search!\n" \
+                "You can tell me if my guess is higher, lower, or correct.\n"
+instructionsLabel = Label(root, text=instructions, font=("Ubuntu", 16), fg=fgc, bg=bgc)
+instructionsLabel.pack(pady=20, padx=20)
+
+# TODO: Start game and reset logic
 
 # Start the application
 if __name__ == "__main__":
