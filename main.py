3ad467ec4b412cfe7ac22bf2f74e1cedb71ecbdf
maooam27 <52970070+maooam27@users.noreply.github.com>
2025-06-16 20:20:29 +0200

    structure for main function

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ root.config(bg="#232A2F")
 bgc = "#232A2F"
 fgc = "#bfbfbf"
 
+
+# Logic for the wizard
+def startWizard():
+    print(numberEntry.get())
+
+
+
 # Title
 title = Label(root, text="PyNumberWizard", font=("Ubuntu", 24), fg=fgc, bg=bgc)
 title.pack(pady=20, anchor="nw", padx=20)
@@ -30,8 +37,11 @@ numberEntry = Entry(instructionsFrame, font=("Ubuntu", 16), fg=fgc, bg="#2C3333"
 numberEntry.grid(pady=10, row=0, column=1, padx=10)
 
 # Button to submit the number
-submitBtn = Button(root, text="Submit", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=lambda: print("Number submitted:", numberEntry.get()))
+submitBtn = Button(root, text="Submit", font=("Ubuntu", 16), fg=fgc, bg="#2C3333", command=startWizard)
 submitBtn.pack(pady=10)
+
+
+# TODO: validate the input
 
 # Start the application
 if __name__ == "__main__":
